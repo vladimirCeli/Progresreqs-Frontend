aef06a0feb814c0eb2cfa3d65b7d1cf449a9da8c
vladimirCeli <vladimir.celi@unl.edu.ec>
2023-12-02 02:37:22 -0500

    cambio de fetch v0.0.1

--- src/hooks/useRefreshToken.jsx
+++ src/hooks/useRefreshToken.jsx
@@ -1,36 +1,40 @@
 import useAuth from './useAuth'
-
+import { useContext,useCallback } from 'react'
 import { refreshApi } from '../Services/Fetch'
-
+import AuthContext
+ from '../context/AuthProvider';
 const useRefreshToken = () => {
-  const { setAuth } = useAuth();
+  const { setAuth } = useContext(AuthContext);
 
-  const refresh = async () => {
-    const response = await fetch(refreshApi, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      credentials: "include",
-    });
-    if (!response.ok) {
-      throw new Error("No se pudo refrescar el token");
-    }
-    const data = await response.json();
-    const username = data?.username;
-    const roles = data?.rol_id;
-    const accessToken = data?.token;
+  const refresh = useCallback(async () => {
+    try {
+      const response = await fetch(refreshApi, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        credentials: 'include',
+      });
 
-    setAuth((prev) => {
-      return {
+      if (!response.ok) {
+        throw new Error('No se pudo refrescar el token');
+      }
+
+      const data = await response.json();
+      const { username, rol_id: roles, token: accessToken } = data;
+
+      setAuth((prev) => ({
         ...prev,
         username,
         accessToken,
         roles,
-      };
-    });
-    return data;
-  };
+      }));
+
+      return data;
+    } catch (error) {
+      console.error(error);
+    }
+  }, [setAuth]);
 
   return refresh;
 };
